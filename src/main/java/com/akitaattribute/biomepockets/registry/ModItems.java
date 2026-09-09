f1e406ae3d7a2f7d5bcfdcc5e6c850a9929e8eda
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2026-09-09 02:24:07 -0400

    Register pocket biome manager item

--- src/main/java/com/akitaattribute/biomepockets/registry/ModItems.java
+++ src/main/java/com/akitaattribute/biomepockets/registry/ModItems.java
@@ -3,25 +3,34 @@ package com.akitaattribute.biomepockets.registry;
 import com.akitaattribute.biomepockets.BiomePockets;
 import com.akitaattribute.biomepockets.item.BiomeTransporterItem;
 import com.akitaattribute.biomepockets.item.BiomeTransporterSelectorItem;
+import com.akitaattribute.biomepockets.item.PocketBiomeManagerItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public final class ModItems {
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BiomePockets.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BiomePockets.MOD_ID);
 
-    public static final RegistryObject<Item> BIOME_TRANSPORTER = ITEMS.register("biome_transporter",
-            () -> new BiomeTransporterItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> BIOME_TRANSPORTER = ITEMS.register(
+            "biome_transporter",
+            () -> new BiomeTransporterItem(new Item.Properties()
+                    .tab(CreativeModeTab.TAB_MISC)
+                    .stacksTo(1)));
 
-    public static final RegistryObject<Item> BIOME_TRANSPORTER_SELECTOR = ITEMS.register("biome_transporter_selector",
-            () -> new BiomeTransporterSelectorItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1)));
+    public static final RegistryObject<Item> BIOME_TRANSPORTER_SELECTOR = ITEMS.register(
+            "biome_transporter_selector",
+            () -> new BiomeTransporterSelectorItem(new Item.Properties()
+                    .tab(CreativeModeTab.TAB_MISC)
+                    .stacksTo(1)));
+
+    public static final RegistryObject<Item> POCKET_BIOME_MANAGER = ITEMS.register(
+            "pocket_biome_manager",
+            () -> new PocketBiomeManagerItem(new Item.Properties()
+                    .tab(CreativeModeTab.TAB_MISC)
+                    .stacksTo(1)));
 
     private ModItems() { }
-
-    public static void register(IEventBus bus) {
-        ITEMS.register(bus);
-    }
 }
