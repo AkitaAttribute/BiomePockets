bfd83e24a386f10f46330d20e6e8cd63089c73a2
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2026-09-09 02:23:40 -0400

    Wire pocket biome manager networking

--- src/main/java/com/akitaattribute/biomepockets/network/NetworkHandler.java
+++ src/main/java/com/akitaattribute/biomepockets/network/NetworkHandler.java
@@ -1,6 +1,7 @@
 package com.akitaattribute.biomepockets.network;
 
 import com.akitaattribute.biomepockets.BiomePockets;
+import com.akitaattribute.biomepockets.world.PocketClaimManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkRegistry;
@@ -10,7 +11,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 import java.util.List;
 
 public final class NetworkHandler {
-    private static final String PROTOCOL = "1";
+    private static final String PROTOCOL = "2";
     private static int packetId = 0;
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
@@ -27,9 +28,23 @@ public final class NetworkHandler {
                 OpenBiomeSelectorPacket::encode, OpenBiomeSelectorPacket::decode, OpenBiomeSelectorPacket::handle);
         CHANNEL.registerMessage(packetId++, SelectBiomePacket.class,
                 SelectBiomePacket::encode, SelectBiomePacket::decode, SelectBiomePacket::handle);
+        CHANNEL.registerMessage(packetId++, OpenPocketManagerPacket.class,
+                OpenPocketManagerPacket::encode, OpenPocketManagerPacket::decode, OpenPocketManagerPacket::handle);
+        CHANNEL.registerMessage(packetId++, PocketManagerActionPacket.class,
+                PocketManagerActionPacket::encode, PocketManagerActionPacket::decode, PocketManagerActionPacket::handle);
     }
 
     public static void openSelector(ServerPlayer player, List<ResourceLocation> biomes) {
         CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), new OpenBiomeSelectorPacket(biomes));
+    }
+
+    public static void openPocketManager(ServerPlayer player) {
+        CHANNEL.send(
+                PacketDistributor.PLAYER.with(() -> player),
+                OpenPocketManagerPacket.from(PocketClaimManager.stateFor(player)));
+    }
+
+    public static void sendPocketManagerAction(PocketClaimManager.Action action) {
+        CHANNEL.sendToServer(new PocketManagerActionPacket(action));
     }
 }
