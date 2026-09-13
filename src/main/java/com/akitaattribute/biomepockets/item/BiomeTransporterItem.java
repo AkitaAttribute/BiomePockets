df982045deb8fd0c94c7a2a617560beb780c0b60
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2026-09-13 03:33:09 -0400

    Use throttled pocket generation for random transport

--- src/main/java/com/akitaattribute/biomepockets/item/BiomeTransporterItem.java
+++ src/main/java/com/akitaattribute/biomepockets/item/BiomeTransporterItem.java
@@ -1,7 +1,7 @@
 package com.akitaattribute.biomepockets.item;
 
 import com.akitaattribute.biomepockets.world.BiomeCatalog;
-import com.akitaattribute.biomepockets.world.PocketInitialSizeManager;
+import com.akitaattribute.biomepockets.world.PocketThrottledInitialGenerator;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -30,7 +30,7 @@ public class BiomeTransporterItem extends Item {
             }
 
             ResourceLocation selected = biomes.get(serverPlayer.getRandom().nextInt(biomes.size()));
-            PocketInitialSizeManager.createAndTeleport(serverPlayer, selected);
+            PocketThrottledInitialGenerator.createAndTeleport(serverPlayer, selected);
         }
         return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
     }
